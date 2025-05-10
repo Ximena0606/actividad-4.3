4c4daebf4aba0f7e6a7bd6d42ab90d073f0e8a9e
Ximena0606 <242310432@itslerdo.edu.mx>
2025-05-10 00:57:52 -0600

    Update usuario.php

--- usuario.php
+++ usuario.php
@@ -1,15 +1,12 @@
 <?php
 abstract class UsuarioCorreo {
-    private static $contadorID= 1;
-    private $id;
-    private $nombre;
-    private $apellidoPaterno;
-    private $apellidoMaterno;
-    private $edad;
-    private $departamento;
-    
-    public function __construct ($nombre, $apellidoPaterno, $apellidoMaterno, $edad, $departamento) {
-        $this->id = self::$contadorID++;
+    protected $nombre;
+    protected $apellidoPaterno;
+    protected $apellidoMaterno;
+    protected $edad;
+    protected $departamento;
+
+    public function __construct($nombre, $apellidoPaterno, $apellidoMaterno, $edad, $departamento) {
         $this->nombre = $nombre;
         $this->apellidoPaterno = $apellidoPaterno;
         $this->apellidoMaterno = $apellidoMaterno;
@@ -17,27 +14,10 @@ abstract class UsuarioCorreo {
         $this->departamento = $departamento;
     }
 
-    public function setNombre($nuevoNombre) {
-        $this->nombre = $nuevoNombre;
+    public function getCorreoInstitucional() {
+        return strtolower($this->nombre . '.' . $this->apellidoPaterno . '@institucion.edu.mx');
     }
 
-    public function setEdad($nuevaEdad) {
-        $this->edad = $nuevaEdad;
-    }
-
-    public function setDepartamento($uevoDepartamento) {
-        $this->departamento = $nuevoDepartamento;
-    }
-
-    public function getID() {
-        return $this->edad;
-    }
-
-    public function getDepartamento() {
-        return $this->departamento;
-    }
-
-    // Método abstracto para forzar que las clases hijas lo implementen
-    abstract public function obtenerNombreCompleto() ;
+    abstract public function obtenerNombreCompleto();
 }
 ?>
